9128ad4ae68f65f9a090f911fdc1e40f745bbd78
davieongora <davidongora@gmail.com>
2025-06-19 06:05:38 +0300

    feat: Update development environment configuration with dummy Firebase keys

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: process.env['FIREBASE_API_KEY'] || '',
-    authDomain: process.env['FIREBASE_AUTH_DOMAIN'] || '',
-    projectId: process.env['FIREBASE_PROJECT_ID'] || '',
-    storageBucket: process.env['FIREBASE_STORAGE_BUCKET'] || '',
-    messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'] || '',
-    appId: process.env['FIREBASE_APP_ID'] || '',
-    measurementId: process.env['FIREBASE_MEASUREMENT_ID'] || ''
+    apiKey: process.env['FIREBASE_API_KEY'] || 'dummy-key-for-build',
+    authDomain: process.env['FIREBASE_AUTH_DOMAIN'] || 'dummy-key-for-build',
+    projectId: process.env['FIREBASE_PROJECT_ID'] || 'dummy-key-for-build',
+    storageBucket: process.env['FIREBASE_STORAGE_BUCKET'] || 'dummy-key-for-build',
+    messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'] || 'dummy-key-for-build',
+    appId: process.env['FIREBASE_APP_ID'] || 'dummy-key-for-build',
+    measurementId: process.env['FIREBASE_MEASUREMENT_ID'] || 'dummy-key-for-build'
   },
 };
