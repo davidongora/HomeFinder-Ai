fd8745ba086df53aff92534a83d25a64b77be1e2
davieongora <davidongora@gmail.com>
2025-06-20 22:19:54 +0300

    feat: Update Firebase environment variables to remove NEXT_PUBLIC prefix

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: process.env['NEXT_PUBLIC_FIREBASE_API_KEY'] || 'dummy-key-for-build',
-    authDomain: process.env['NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN'] || 'dummy-key-for-build',
-    projectId: process.env['NEXT_PUBLIC_FIREBASE_PROJECT_ID'] || 'dummy-key-for-build',
-    storageBucket: process.env['NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET'] || 'dummy-key-for-build',
-    messagingSenderId: process.env['NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID'] || 'dummy-key-for-build',
-    appId: process.env['NEXT_PUBLIC_FIREBASE_APP_ID'] || 'dummy-key-for-build',
-    measurementId: process.env['NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID'] || 'dummy-key-for-build'
+    apiKey: process.env['apiKey'] || 'dummy-key-for-build',
+    authDomain: process.env['authDomain'] || 'dummy-key-for-build',
+    projectId: process.env['projectId'] || 'dummy-key-for-build',
+    storageBucket: process.env['storageBucket'] || 'dummy-key-for-build',
+    messagingSenderId: process.env['messagingSenderId'] || 'dummy-key-for-build',
+    appId: process.env['appId'] || 'dummy-key-for-build',
+    measurementId: process.env['measurementId'] || 'dummy-key-for-build'
   },
 };
