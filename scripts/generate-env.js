e15ccec96e13c14c1e544cba835b90edc1c955a2
davieongora <davidongora@gmail.com>
2025-06-20 23:57:40 +0300

    feat: Update Firebase environment configuration to use environment variables

--- scripts/generate-env.js
+++ scripts/generate-env.js
@@ -10,18 +10,25 @@ let template = fs.readFileSync(templatePath, 'utf-8');
 console.log('reading Firebase environment.prod.ts generated');
 
 template = template
-    .replace('__API_KEY__', process.env["apiKey"] || '')
-    .replace('__AUTH_DOMAIN__', process.env["authDomain"] || '')
-    .replace('__PROJECT_ID__', process.env["projectId"] || '')
-    .replace('__STORAGE_BUCKET__', process.env["storageBucket"] || '')
-    .replace('__MESSAGING_SENDER_ID__', process.env["messagingSenderId"] || '')
-    .replace('__APP_ID__', process.env["appId"] || '')
-    .replace('__MEASUREMENT_ID__', process.env["measurementId"] || '');
+    .replace('__API_KEY__', process.env["NEXT_PUBLIC_FIREBASE_API_KEY"] || '')
+    .replace('__AUTH_DOMAIN__', process.env["NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN"] || '')
+    .replace('__PROJECT_ID__', process.env["NEXT_PUBLIC_FIREBASE_PROJECT_ID"] || '')
+    .replace('__STORAGE_BUCKET__', process.env["NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET"] || '')
+    .replace('__MESSAGING_SENDER_ID__', process.env["NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID"] || '')
+    .replace('__APP_ID__', process.env["NEXT_PUBLIC_FIREBASE_APP_ID"] || '')
+    .replace('__MEASUREMENT_ID__', process.env["NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID"] || '');
+
 
 fs.writeFileSync(envPath, template);
 console.log('✅ Firebase environment.prod.ts generated');
 
 console.log('📦 Firebase Config:', {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
     // ... log the rest if needed
   });
